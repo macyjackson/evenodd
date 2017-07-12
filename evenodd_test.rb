12d7983c97403e2758b7975aecf9eea9481bcf6e
Macy <macyjackson@ymail.com>
2017-07-12 14:22:50 -0400

    addition intitial file

--- evenodd_test.rb
+++ evenodd_test.rb
@@ -4,7 +4,10 @@ require_relative "evenodd.rb"
 class TestEvenOdd < Minitest::Test
 
 	def test_assert_that_one_equals_1
-		assert_equal(1,1)
+		assert_equal(1, 1)
 	end
 
+	def test_input_1_returns_odd
+		assert_equal("odd", evenoddfunction(1))
+	end
 end
